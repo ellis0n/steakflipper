243371db4fd88de583395aac7b0fc4ed9386cbfc
ellis0n <ellisonglenmay@gmail.com>
2022-08-10 23:54:02 -0230

    aug 10 pm

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      {" "}
+      <p>This program was written in React </p>
+      <p>
+        This placeholder text should be replaced by some type of functional code
+      </p>
+      <p>Perhaps steak facts? user login? Share user pics of Steaks?</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,7 @@
+// Make an array that holds the values of the button that will be modified
+
+export const steakArray = [
+  {
+    value: null,
+  },
+];
